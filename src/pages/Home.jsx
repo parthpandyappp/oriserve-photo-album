0b20ccd5e898f5cf0d2261f1dbbab475876d8d12
parthpandyappp <parthpandyappp@gmail.com>
2023-06-29 02:33:11 +0530

    feat: infinite scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,50 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [pics, setPics] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    (async () => {
-      const res = await axios.get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${process.env.REACT_APP_FLICKR_API_KEY}&format=json&nojsoncallback=1&per_page=30&page=1&safe_search=1`
-      );
-      setPics(res.data.photos.photo);
-    })();
-  }, []);
+    const fetchPhotos = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${process.env.REACT_APP_FLICKR_API_KEY}&format=json&nojsoncallback=1&per_page=5&page=${page}&safe_search=1`
+        );
+        const newPics = res.data.photos.photo;
+        setPics((prevPics) => [...prevPics, ...newPics]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    fetchPhotos();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [page]);
 
   return (
     <main className="py-3 px-4 h-full w-full">
       <div className="flex flex-wrap w-full gap-1 grow">
-        {pics.length > 0 &&
-          pics.map((pic) => {
-            return (
-              <div className="grow w-1/4 h-96">
-                <img
-                  src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_c.jpg`}
-                  className="h-full w-full  object-cover"
-                  alt={pic.title}
-                />
-              </div>
-            );
-          })}
+        {pics.map((pic) => (
+          <div key={pic.id} className="grow w-1/4 h-96">
+            <img
+              src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_c.jpg`}
+              className="h-full w-full object-cover"
+              alt={pic.title}
+            />
+          </div>
+        ))}
       </div>
     </main>
   );
